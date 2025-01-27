fd5a342c7992ddf9303886ce4918e77ac7ed811d
mritunjay singh <a34mritunjaysingh@gmail.com>
2025-01-27 19:45:35 +0530

    pcap Shuks

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,15 +1,32 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const PORT = 8000;
-app.use(express_1.default.json());
-app.get('/', () => {
-    console.log("Hello i am up");
+// import express from 'express';
+// import pcap from 'pcap';
+// const app=express();
+// const PORT=8000;
+// app.use(express.json());
+// app.get('/',()=>{
+//     console.log("Hello i am up")
+// })
+// const interfaceName='wlp0s20f3';
+// // const session=pcap.createSession(interfaceName);
+// try {
+//     const session = pcap.createSession(interfaceName);
+//     console.log('Packet capture started on interface:', interfaceName);
+//   } catch (error) {
+//     console.error('Failed to create packet capture session:', error);
+//   }
+// // console.log(session)
+// app.listen(PORT,()=>{
+//     console.log('Server is running on port',PORT);
+// })
+var pcap = require('pcap'), tcp_tracker = new pcap.TCPTracker(), pcap_session = pcap.createSession('en0', { filter: "ip proto \\tcp" });
+tcp_tracker.on('session', function (session) {
+    console.log("Start of session between " + session.src_name + " and " + session.dst_name);
+    session.on('end', function (session) {
+        console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
+    });
 });
-app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+pcap_session.on('packet', function (raw_packet) {
+    var packet = pcap.decode.packet(raw_packet);
+    tcp_tracker.track_packet(packet);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import pcap from 'pcap';
+
 const app=express();
 const PORT=8000;
 app.use(express.json());
 app.get('/',()=>{
     console.log("Hello i am up")
 })
+const interfaceName='wlp0s20f3';
+// const session=pcap.createSession(interfaceName);
+try {
+    const session = pcap.createSession(interfaceName,  { 
+        filter: 'ip', 
+        promiscuous: true, 
+        buffer_size: 10 * 1024 * 1024 // 10 MB buffer
+      });
+    console.log('Packet capture started on interface:', interfaceName);
+  } catch (error) {
+    console.error('Failed to create packet capture session:', error);
+  }
+  
+// console.log(session)
 app.listen(PORT,()=>{
     console.log('Server is running on port',PORT);
 })
